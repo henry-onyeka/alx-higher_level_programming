802309edf56948ad84d5c55ad8bb18aba30eb3e5
MICHAEL HENRY ONYEKA <hmike54@gmail.com>
2022-12-21 17:06:49 +0000

    Update 2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0)
-  rise = 0
- for i in range(x):
-   try:
-    print("{:d}",.format(my_list[i]), end='')
+     rise = 0
+     for i in range(x):
+        try:
+           print("{:d}",.format(my_list[i]), end='')
        
-   except TypeError:
-     pass
-   except ValueError:
-     pass
-   else:
-     rise += 1
-   print()
-   return(rise)
+         except TypeError:
+           pass
+         except ValueError:
+           pass
+         else:
+           rise += 1
+     print()
+     return(rise)
