b5726388cd856f20913ae7ebfac8877cc38794b4
MICHAEL HENRY ONYEKA <hmike54@gmail.com>
2022-12-21 17:15:18 +0000

    Update 1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer()
-   try:
+    try:
       print("{:d}".format(value))
-   except:
+    except:
       return (False)
-   else:
+    else:
       return (True)
