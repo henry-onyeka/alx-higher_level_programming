c6b9d792c13711148413e7bb819abcb9b735e5b5
henry-onyeka <hmike54@gmail.com>
2022-12-25 22:42:25 +0000

    trasversing

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
+
 """Define a class Square."""
 
+
 class Square:
+
     """Class Square that defines a square.
     """
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
+
+"""Define a class Square."""
+
+
 class Square:
-     """Class Square that defines a square object
+
+    """Class Square that defines a square object
     """
-    def __init__(self,size):
-       """Initialize method that stores the size of the square
+
+    def __init__(self, size):
+
+        """Initialize method that stores the size of the square
         Args:
             param1 (int): size of the square
         """
