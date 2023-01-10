cd9e74e341cff5f713a710e36bb2d05338b828d5
henry-onyeka <hmike54@gmail.com>
2023-01-10 02:20:54 +0000

    tests

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+class MyList(list):
+    
+
+    """ Class that inherits the attributes references of class list
+    Args:
+        list: class list
+    """
+
+
+    
+    def print_sorted(self):
+       '''
+       print sorted list
+       '''
+        sorted_list = self.copy()
+        sorted_list.sort()
+        print(sorted_list) 
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+inheritance is all about passing parent classes to child class 
