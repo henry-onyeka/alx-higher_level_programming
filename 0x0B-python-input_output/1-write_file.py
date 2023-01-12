dc85315025d7083c37c9f6ab58480cb6af0fd451
MICHAEL HENRY ONYEKA <hmike54@gmail.com>
2023-01-12 00:39:18 +0000

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
-"""number_of_lines
+""" Module that contains a function that reads n lines of a text file
 """
 
 
-
-def number_of_lines(filename=""):
-    """Takes in str filename to read the number of lines
+def read_lines(filename="", nb_lines=0):
+    """ Function that reads from a file and prints its number of lines
+    Args:
+        filename: filename
+        nb_lines: number of lines to print
+    Raises
+        Exception: when the file can be opened
     """
 
-    with open(filename, encoding="utf-8") as readFile:
-        lines = 0
-        while True:
-            line = readFile.readline()
-            if not line:
-                break
-            lines += 1
-        return lines
+    with open(filename, 'r', encoding="utf-8") as f:
+        if nb_lines <= 0:
+            read_data = f.read()
+            print(read_data, end='')
+        else:
+            n_lines = 0
+            for line in f:
+                print(line, end='')
+                n_lines += 1
+                if n_lines == nb_lines:
+                    break
