179b1f9c22d22d4e2b92afc4e3730a8d6fc539be
henry-onyeka <hmike54@gmail.com>
2023-01-12 00:29:11 +0000

    created

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,18 +1,13 @@
-#!/usr/bin/python3
-""" Module that contains a function that returns the number of lines
-    of a text file
-"""
-
-
-def number_of_lines(filename=""):
-    """ Function that reads from a file and prints its number of lines
-    Args:
-        filename: filename
-    Raises
-        Exception: when the file can be opened
-    """
-    xters = 0
-    with open('filename', 'r', encoding = "utf-8") as something:
-        for line in something:
-            xters += 1
-    return xters
+  6 
+  7 def number_of_lines(filename=""):
+  8     """ Function that reads from a file and prints its number of lines
+  9     Args:
+ 10         filename: filename
+ 11     Raises
+ 12         Exception: when the file can be opened
+ 13     """
+ 14     xters = 0
+ 15     with open('filename', 'r', encoding = "utf-8") as something:
+ 16         for line in something: 
+ 17             xters += 1
+ 18     return xters
