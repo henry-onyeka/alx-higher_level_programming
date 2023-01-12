5c39e22b03c4ee33168577b78c79451a6bfe3c95
henry-onyeka <hmike54@gmail.com>
2023-01-12 01:18:34 +0000

    created

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,13 +1,13 @@
-#!/usr/bin/python3
-'''
-working json conversion
-'''
-
-import json
-
-def to_json_string(my_obj):
-    '''
-    returning a string in json format
-    '''
-    
-    return (json.dumps(my_obj))
+  1 #!/usr/bin/python3
+  2 '''
+  3 working json conversion
+  4 '''
+  5 
+  6 import json
+  7 
+  8 def to_json_string(my_obj):
+  9     '''
+ 10     returning a string in json format
+ 11     '''
+ 12 
+ 13     return (json.dumps(my_obj))
