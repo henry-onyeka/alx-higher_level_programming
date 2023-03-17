d89eb47f4fb2b615a7b17d2c46e520a75ca5ff8b
henry-onyeka <hmike54@gmail.com>
2023-03-17 00:56:10 +0000

    javascript

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-const msg = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i in msg) {
-  console.log(msg[i]);
+
+const msg = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const i of msg) {
+  console.log(i);
 }
