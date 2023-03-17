5662cb975057c52d732e61e22e027ca9fe4ac404
henry-onyeka <hmike54@gmail.com>
2023-03-17 00:41:27 +0000

    sjavascript

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 const word = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 let o = 0;
-while(o < word.length)
-{
-    console.log(word[o]);
-    o++;
+while (o < word.length) {
+  console.log(word[o]);
+  o++;
 }
--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
-let ans = parseInt(process.argv[2]);
-if(Number.isNaN(ans))
-{
-    console.log("Missing number of occurrences");
-}
-else
-{
-    for(i = 0; i < ans; i++)
-    {
-    console.log("C is fun");
-    }
+const ans = parseInt(process.argv[2]);
+
+if (Number.isNaN(ans)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < ans; i++) {
+    console.log('C is fun');
+  }
 }
