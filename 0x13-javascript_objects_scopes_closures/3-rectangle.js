a145664754a963feabab316d3c6dfd31e626cb67
henry-onyeka <hmike54@gmail.com>
2023-03-18 00:04:56 +0000

    creating a class rectangle that defines rectangle with different instances

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+class Rectangle{
+    constructor(w,h){
+        if((w > 0) && (h > 0))
+        {
+        this.height = h;
+        this.width = w;
+    }  
+}
+ print(){
+    let i = 0;
+    let p = 0;
+    for(i = 0; i < this.height; i ++){
+        let ph ='';
+        for(p = 0; p < this.width; p++){
+             ph += 'X';
+        }
+        console.log(ph);
+    }
+}
+}
+module.exports = Rectangle;
---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+class Rectangle{
+    constructor(w,h){
+        if((w > 0) && (h > 0))
+        {
+        this.height = h;
+        this.width = w;
+    }  
+}
+ print(){
+    let i = 0;
+    let p = 0;
+    for(i = 0; i < this.height; i ++){
+        let ph ='';
+        for(p = 0; p < this.width; p++){
+             ph += 'X';
+        }
+        console.log(ph);
+    }
+}
+
+rotate(){
+    let temp = this.width;
+    this.width = this.height ;
+    this.height = temp;
+
+}
+
+double(){
+    this.width *= 2;
+    this.height * 2;
+}
+}
+module.exports = Rectangle;
+
